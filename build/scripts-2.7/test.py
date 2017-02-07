607d8d368c8f5a053a005684a3c6ccfe787655a3
Filip Tomic <filip.tomic@tegetdot.com>
2017-02-07 09:44:40 +0100

    Init files

---
+++ build/scripts-2.7/test.py
@@ -0,0 +1,10 @@
+from Mammals import Mammals
+from Birds import Birds
+
+# Create an object of Mammals class & call a method of it
+myMammal = Mammals()
+myMammal.print_members()
+
+# Create an object of Birds class & call a method of it
+myBird = Birds()
+myBird.print_members()
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='animals',    # This is the name of your PyPI-package.
+    version='0.3',                          # Update the version number for new releases
+    scripts=['test.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)
